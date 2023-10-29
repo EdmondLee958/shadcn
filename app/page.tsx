400baddce6aada2f88d0c9a726abb3a1e97b883a
EdmondLee958 <edmondlee958@gmail.com>
2023-10-28 19:54:37 -0700

    e version

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
 import { buttonVariants } from "@/components/ui/button";
@@ -8,22 +9,93 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Separator } from "@/components/ui/separator";
 
-export function TextareaWithButton() {
+export function TextareaWithButton(): JSX.Element {
+  const [text, setText] = useState<string>(""); // State variable to store the current text
+  const [history, setHistory] = useState<string[]>([]); // Specify the type as an array of strings
+
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    setText(e.target.value); // Update the current text as the user types
+  };
+
+  const handleTextareaEnter = (e: KeyboardEvent<HTMLTextAreaElement>): void => {
+    if (e.key === 'Enter' && text.trim() !== '') {
+      e.preventDefault(); // Prevent the default Enter key behavior
+      sendMessage();
+    }
+  };
+
+  const sendMessage = (): void => {
+    if (text.trim() !== '') {
+      // When the user clicks Send or presses Enter and the text is not empty
+      const updatedHistory = [...history, text];
+      const limitedHistory = updatedHistory.slice(-5); // Limit to the most recent 5 messages
+      setHistory(limitedHistory);
+      setText(""); // Clear the current text
+    }
+  };
+
   return (
-    <div className="grid w-full gap-2">
+    <div className="grid w-full gap-2 relative">
       <div className="relative">
         <Textarea
-          placeholder="Type your prompt here."
+          placeholder="Type your message here."
+          value={text}
+          onChange={handleTextareaChange}
+          onKeyDown={handleTextareaEnter}
           className="w-full"
         />
-        <Button className="h-10 absolute right-4 top-1/2 transform -translate-y-1/2">Send</Button>
+        <Button
+          onClick={sendMessage}
+          className="absolute right-4 top-1/2 transform -translate-y-1/2 h-10 bg-blue-500 text-white"
+        >
+          Send
+        </Button>
+      </div>
+      <div className="history-scroll">
+        {history.map((entry, index) => (
+          <div key={index}>{entry}</div>
+        ))}
       </div>
     </div>
   );
 }
 
-export function TextareaDemo() {
-  return <Textarea placeholder="Type patient's background here." />;
+
+export function TextareaDemo(): JSX.Element {
+  const [text, setText] = useState<string>(""); // State variable to store the current text
+  const [history, setHistory] = useState<string[]>([]); // Specify the type as an array of strings
+
+  const handleTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    setText(e.target.value); // Update the current text as the user types
+  };
+
+  const handleTextareaEnter = (e: KeyboardEvent<HTMLTextAreaElement>): void => {
+    if (e.key === 'Enter' && text.trim() !== '') {
+      e.preventDefault(); // Prevent the default Enter key behavior
+      // When the user presses Enter and the text is not empty
+      const updatedHistory = [...history, text];
+      const limitedHistory = updatedHistory.slice(-5); // Limit to the most recent 5 messages
+      setHistory(limitedHistory);
+      setText(""); // Clear the current text
+    }
+  };
+
+  return (
+    <div className="grid w-full gap-2">
+      <Textarea
+        placeholder="Type patient's background here."
+        value={text}
+        onChange={handleTextareaChange}
+        onKeyDown={handleTextareaEnter}
+        className="w-full"
+      />
+      <div className="history-scroll">
+        {history.map((entry, index) => (
+          <div key={index}>{entry}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default function IndexPage() {
