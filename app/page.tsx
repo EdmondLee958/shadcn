86199647562f62e4966d4c52e03d9d86cf8971ef
EdmondLee958 <edmondlee958@gmail.com>
2023-10-28 18:43:44 -0700

    final draft of ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,42 @@
-import Link from "next/link"
-import { siteConfig } from "@/config/site"
+'use client'
 
-import { buttonVariants } from "@/components/ui/button"
+import { useEffect } from 'react'
+import Link from "next/link";
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea"
-
+import { Textarea } from "@/components/ui/textarea";
+import { Separator } from "@/components/ui/separator";
 
 export function TextareaWithButton() {
   return (
     <div className="grid w-full gap-2">
-      <Textarea placeholder="Type your prompt here." />
-      <Button>Send message</Button>
+      <div className="relative">
+        <Textarea
+          placeholder="Type your prompt here."
+          className="w-full"
+        />
+        <Button className="h-10 absolute right-4 top-1/2 transform -translate-y-1/2">Send</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export function TextareaDemo() {
-  return <Textarea placeholder="Type patient's background here." />
+  return <Textarea placeholder="Type patient's background here." />;
 }
-
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex gap-4 justify-center items-center" style={{ minHeight: '100vh', flexDirection: 'column' }}>
+    <section className="container mx-auto grid items-center gap-4 pb-8 pt-8 md:py-8">
+      <div
+        className="flex gap-2 justify-center items-center ml-8 mr-8"
+        style={{ minHeight: "100vh", flexDirection: "column" }}
+      >
         <TextareaDemo></TextareaDemo>
+        <Separator className="bg-gray-300 h-1 w-full"></Separator>
         <TextareaWithButton />
-
       </div>
     </section>
   );
-};
+}
